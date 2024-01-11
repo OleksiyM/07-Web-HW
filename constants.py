2d1fe75e1c59d33972b21d798d56af130bf58c6a
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-01-11 18:55:11 +0200

    init

---
+++ constants.py
@@ -0,0 +1,20 @@
+MIN_GRADE = 1
+MAX_GRADE = 5
+
+DAYS_DELTA = 120
+
+tables_list = ('groups', 'students', 'professors', 'subjects', 'grades')
+
+tables_str = ', '.join(tables_list)
+
+students_in_a_group_count = 15
+# random groups names
+groups = ('group_1', 'group_2', 'group_3')
+groups_list = [('group_1',), ('group_2',), ('group_3',)]
+
+# random subjects
+subjects = ('math', 'physics', 'chemistry', 'biology', 'history')
+
+# random professors name
+professors = ('Professor Emily Thompson', 'Professor William Smith', 'Professor Jessica Zhang',
+              'Professor Charles Harris', 'Professor Nicole Wilson')
