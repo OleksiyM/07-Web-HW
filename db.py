b8159e3ccfe10e31ee3860d76d7e8c612e38d4a2
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-01-11 13:51:58 +0200

    error handling added

--- db.py
+++ db.py
@@ -1,24 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from models import Base
 
 USER = 'postgres'
 PASSWORD = '123456'
 HOST = 'localhost'
 PORT = '5432'
-DATABASE = 'dbase'
+DATABASE = 'postgres'
 
 
 def get_connection_string():
     return f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
 
-def create_db():
-    engine = create_engine(get_connection_string())
-    Session = sessionmaker(bind=engine)
-    session = Session()
+def create_db(engine):
     Base.metadata.create_all(engine)
+    return 'Tables were created'
+
+
+def drop_db(engine):
+    Base.metadata.drop_all(engine)
+    return 'Tables were dropped'
 
 
 if __name__ == '__main__':
-    create_db()
+    print(get_connection_string())
+    try:
+        engine = create_engine(get_connection_string())
+        Session = sessionmaker(bind=engine)
+        session = Session()
+    except Exception as e:
+        print(f'Error:  {e}')
+
+    print(create_db(engine))
+
+    # print(drop_db(engine))
