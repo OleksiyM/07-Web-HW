9ccb6d3b94e0e53489da593881a923a38fa8f85c
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-01-11 15:36:59 +0200

    db connection settings

--- db.py
+++ db.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Base
-
 USER = 'postgres'
 PASSWORD = '123456'
 HOST = 'localhost'
@@ -14,25 +12,11 @@ def get_connection_string():
     return f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
 
-def create_db(engine):
-    Base.metadata.create_all(engine)
-    return 'Tables were created'
+URI = get_connection_string()
 
-
-def drop_db(engine):
-    Base.metadata.drop_all(engine)
-    return 'Tables were dropped'
-
-
-if __name__ == '__main__':
-    print(get_connection_string())
-    try:
-        engine = create_engine(get_connection_string())
-        Session = sessionmaker(bind=engine)
-        session = Session()
-    except Exception as e:
-        print(f'Error:  {e}')
-
-    print(create_db(engine))
-
-    # print(drop_db(engine))
+try:
+    engine = create_engine(URI, echo=True, future=True, pool_size=5, max_overflow=0, pool_pre_ping=True)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+except Exception as e:
+    print(f'Error:  {e}')
